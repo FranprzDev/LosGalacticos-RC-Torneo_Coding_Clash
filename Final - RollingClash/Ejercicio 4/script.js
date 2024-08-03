ed7b277e9e717bdf262ca832f4346041e8bff9fa
FranprzDev - Notebook <franutn23@gmail.com>
2024-08-03 12:32:03 -0600

    Add the finish of the exercise 5

--- Final - RollingClash/Ejercicio 4/script.js
+++ Final - RollingClash/Ejercicio 4/script.js
@@ -1,1 +1,84 @@
-alert('hola')
+/*
+
+# Combinaciones de Teléfono
+
+Implementa una función que, dada una cadena que contiene solo dígitos del 2 al 9, devuelva todas las combinaciones posibles de letras que los números podrían representar en un teclado de teléfono. El mapeo de dígitos a letras es el mismo que en un teléfono tradicional (por ejemplo, 2 corresponde a ["a", "b", "c"]).
+
+### Objetivo
+
+El objetivo es generar todas las combinaciones de letras que pueden formarse a partir de la secuencia de dígitos dada.
+
+Ejemplo de entrada y salida
+Entrada:
+
+```js
+const digitos = "23";
+```
+
+Salida:
+
+```js
+["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
+```
+
+### Pistas
+
+- Usa un mapeo de los dígitos a sus correspondientes letras.
+- Utiliza recursión para generar todas las combinaciones posibles.
+
+*/
+
+const digitsParse = [{
+    number: 2,
+    alphabet: ['a', 'b', 'c']
+},
+{
+    number: 3,
+    alphabet: ['d', 'e', 'f']
+},
+{
+    number: 4,
+    alphabet: ['g', 'h', 'i']
+},
+{
+    number: 5,
+    alphabet: ['j', 'k', 'l']
+},
+{
+    number: 6,
+    alphabet: ['m', 'n', 'o']
+},
+{
+    number: 7,
+    alphabet: ['p', 'q', 'r']
+},
+{
+    number: 8,
+    alphabet: ['s', 't', 'u']
+},
+{
+    number: 9,
+    alphabet: ['v', 'w', 'x']
+}
+]
+
+const recursiveCombinations(digit, length) {
+    if(digit < 2 || digit > 10) return null
+    // return digitsParse.filter((digitArray) => {
+    //     if(digit === digitArray.number) return digitArray.alphabet
+    //     return null 
+    // })
+}
+
+function getAllCombinations(digits) {
+    if(digits.length <= 0) return null
+    const combinations = []
+    let length = digits.length
+    for(let i = 0; i < digits.length; i++){
+        combinations = recursiveCombinations(digits[i])
+    }
+}
+
+console.log(getAllCombinations("23")) 
+// La salida deberia ser:
+// ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
--- Final - RollingClash/Ejercicio 5/script.js
+++ Final - RollingClash/Ejercicio 5/script.js
@@ -1,1 +1,44 @@
-alert('hola')
+/*
+
+# Juego de Palabras
+
+Implementa una función que reciba un arreglo de palabras y un número entero n. La función debe devolver un nuevo arreglo que contenga solo las palabras que tienen exactamente n caracteres.
+
+### Objetivo
+
+El objetivo es filtrar las palabras del arreglo original para que solo queden aquellas que tienen una longitud específica.
+
+Ejemplo de entrada y salida
+Entrada:
+
+```js
+const palabras = ["casa", "perro", "gato", "elefante", "pez"];
+const n = 4;
+```
+
+Salida:
+
+```js
+["casa", "gato"];
+```
+
+### Pistas
+
+- Utiliza un bucle para recorrer el arreglo de palabras.
+- Usa una condición para verificar la longitud de cada palabra.
+- Agrega las palabras que cumplen la condición a un nuevo arreglo.
+*/
+
+
+const palabras = ["casa", "perro", "gato", "elefante", "pez"];
+
+function findWordByCharacterLenght(words, n) {
+    return words.filter((word) => word.length === n);
+}
+  
+// Casos de prueba:
+console.log(findWordByCharacterLenght(["casa", "perro", "gato", "elefante", "pez"], 4)) /* Casa y perro */
+console.log(findWordByCharacterLenght(["casa", "perro", "gato", "elefante", "pez"], 3)) /* Pez */
+
+
+  console.log(palabrasConLongitud(palabras, n)); // Salida: ["casa", "gato"
