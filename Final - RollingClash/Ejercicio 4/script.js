a0a3df96d825a15124fde8a82b0f1a96b07f223d
FranprzDev - Notebook <franutn23@gmail.com>
2024-08-03 12:49:54 -0600

    Comment the exercise 4 because is too hard

--- Final - RollingClash/Ejercicio 4/script.js
+++ Final - RollingClash/Ejercicio 4/script.js
@@ -28,57 +28,11 @@ Salida:
 
 */
 
-const digitsParse = [{
-    number: 2,
-    alphabet: ['a', 'b', 'c']
-},
-{
-    number: 3,
-    alphabet: ['d', 'e', 'f']
-},
-{
-    number: 4,
-    alphabet: ['g', 'h', 'i']
-},
-{
-    number: 5,
-    alphabet: ['j', 'k', 'l']
-},
-{
-    number: 6,
-    alphabet: ['m', 'n', 'o']
-},
-{
-    number: 7,
-    alphabet: ['p', 'q', 'r']
-},
-{
-    number: 8,
-    alphabet: ['s', 't', 'u']
-},
-{
-    number: 9,
-    alphabet: ['v', 'w', 'x']
-}
-]
+/* BUSCAR ACERCA DE LA IMPLEMENTACIÓN DE ESTE ALGORITMO,
+HAY QUE HACER UNA ESPECIE DE BACKTRACK O ALGO PARECIDO
+HAY QUE IR FILTRANDOLO, DEJO LOS PASOS AQUI ABAJO; PASO A OTRO
+EJERCICIO POR QUE ESTE NOS TOMARÁ MÁS TIEMPO... */
 
-const recursiveCombinations(digit, length) {
-    if(digit < 2 || digit > 10) return null
-    // return digitsParse.filter((digitArray) => {
-    //     if(digit === digitArray.number) return digitArray.alphabet
-    //     return null 
-    // })
-}
-
-function getAllCombinations(digits) {
-    if(digits.length <= 0) return null
-    const combinations = []
-    let length = digits.length
-    for(let i = 0; i < digits.length; i++){
-        combinations = recursiveCombinations(digits[i])
-    }
-}
-
-console.log(getAllCombinations("23")) 
+// console.log(getAllCombinations("23")) 
 // La salida deberia ser:
 // ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
